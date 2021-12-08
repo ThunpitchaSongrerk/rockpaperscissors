0dc95db057ff165b9469cea3956e41cd3d3ba427
Glitch (rock-paper-scissors-proje) <none>
2021-12-08 23:19:49 +0000

    finished with conditionals

--- js/script.js
+++ js/script.js
@@ -1,88 +1,89 @@
-//for some unkown reason we if we use all lowercase it gives all 3 results \/(-_-)\/
-//NVM william fixed it
 let computerChoiceNum = ["rock", "paper", "scissors"];
+let totalWins = 0;
+let totalTies = 0;
+let totalLoses = 0;
+let totalGames = 0;
+let winRate = totalWins / totalGames;
 
-let randomNumber = 0;
-
-// User clicks button
 
 // Thunpitcha's function
 // Docs: https://docs.google.com/document/d/1gq7oUif59sKDInbJrDTIeSQBaOlGb5ri06b2-noWWNI/edit#heading=h.x4lnrrw34ze2
 function getRandomComputerChoice() {
-  return computerChoiceNum;
-  randomNumber = Math.floor(Math.random() * 3);
-  $(".computerChoice").text(computerChoiceNum[randomNumber]);
+  let randomNumber = Math.floor(Math.random() * 3);
+
+  let choice;
+
+  if (randomNumber === 0) {
+    choice = "rock";
+  } else if (randomNumber === 1) {
+    choice = "scissors";
+  } else {
+    choice = "paper";
+  }
+
+  return choice; // value = "rock", "paper", "scissors"
 }
 
+$(".clear").click(function() {
+  $(".input").val("");
+  $(".userChoice").text("");
+  $(".result").text("");
+  $(".computerChoice").text("");
+});
+
 $(".play").click(function() {
+
+  let result;
+  totalGames = totalGames + 1;
+
+  let choices = getRandomComputerChoice();
+
   let user_choice = $(".input").val();
 
   $(".userChoice").text(user_choice);
+
   user_choice = user_choice.toLowerCase();
-  randomNumber = Math.floor(Math.random() * 3);
 
-  $(".computerChoice").text(computerChoiceNum[randomNumber]);
+  $(".computerChoice").text(choices);
 
-  // Here
-  console.log(user_choice);
-  console.log(computerChoiceNum[randomNumber]);
-
-  if (user_choice == "rock" && computerChoiceNum[randomNumber] == "scissors") {
-    /// result = ...;
-    let result = "You Won!";
-    return result;
-  } else if (
-    user_choice == "rock" &&
-    computerChoiceNum[randomNumber] == "rock"
-  ) {
-    let result = "You Tied!";
-    return result;
-  } else if (
-    user_choice == "rock" &&
-    computerChoiceNum[randomNumber] == "paper"
-  ) {
-    let result = "You Lost!";
-    return result;
-  } else if (
-    user_choice == "paper" &&
-    computerChoiceNum[randomNumber] == "rock"
-  ) {
-    let result = "You Won!";
-    return result;
-  } else if (
-    user_choice == "paper" &&
-    computerChoiceNum[randomNumber] == "scissors"
-  ) {
-    let result = "You Lost!";
-    return result;
-  } else if (
-    user_choice == "paper" &&
-    computerChoiceNum[randomNumber] == "paper"
-  ) {
-    let result = "You Tied!";
-    return result;
-  } else if (
-    user_choice == "scissors" &&
-    computerChoiceNum[randomNumber] == "scissors"
-  ) {
-    let result = "You Tied!";
-    return result;
-  } else if (
-    user_choice == "scissors" &&
-    computerChoiceNum[randomNumber] == "rock"
-  ) {
-    let result = "You Lost!";
-    return result;
-  } else if (
-    user_choice == "scissors" &&
-    computerChoiceNum[randomNumber] == "paper"
-  ) {
-    let result = "You Won!";
-    return result;
+  if (user_choice == "rock" && choices == "scissors") {
+    result = "You Won!";
+  } else if (user_choice == "rock" && choices == "rock") {
+    result = "You Tied!";
+  } else if (user_choice == "rock" && choices == "paper") {
+    result = "You Lost!";
+  } else if (user_choice == "paper" && choices == "rock") {
+    result = "You Won!";
+  } else if (user_choice == "paper" && choices == "scissors") {
+    result = "You Lost!";
+  } else if (user_choice == "paper" && choices == "paper") {
+    result = "You Tied!";
+  } else if (user_choice == "scissors" && choices == "scissors") {
+    result = "You Tied!";
+  } else if (user_choice == "scissors" && choices == "rock") {
+    result = "You Lost!";
+  } else if (user_choice == "scissors" && choices == "paper") {
+    result = "You Won!";
   } else {
     $(".result").append(`<p>Your input is invalid, try again.<p>`);
   }
-  // use 'result'
-  console.log(result);
-  $(".result").append(`<p>${result}</p>`);
+  for (totalWins = 0; totalWins <= totalGames;){
+      $(".wins").text("You have won" + totalWins + "times");
+      totalWins = totalWins +1;
+  }
+  for (totalLoses = 0; totalLoses <= totalGames; ){
+    $(".loses").text("You have lost" + totalLoses + "times");
+    totalLoses = totalLoses +1;
+  }
+  for (totalTies = 0; totalTies <= totalGames; ){
+    $(".ties").text("You have tied" + totalTies + "times");
+    totalTies = totalTies +1;
+}
+  
+  
+    // use 'result'
+
+  $(".result").text(result);
+  $(".winRate").text("Your win rate is" + winRate + "percent");
+  $(".games").text("You have played" + totalGames + "times");
 });
